c68dd4a35c6ed50170265c84cde96c8cef4254fa
ayoubgm <ayoubguismi@outlook.com>
2023-08-02 02:48:11 +0100

    Add configuration files of tailwind and postcss and setup also the vuex store

---
+++ src/store/index.ts
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex';
+
+export default createStore({
+  state: {
+    // Your state variables here
+  },
+  mutations: {
+    // Your mutation functions here
+  },
+  actions: {
+    // Your action functions here
+  },
+  modules: {
+    // You can divide your store into modules if needed
+  },
+});
